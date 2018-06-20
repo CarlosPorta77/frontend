3d04e013d08a3952a727b7475bd437e09aa65453
Turco <amado.yamil@gmail.com>
2018-06-19 21:43:16 -0300

    clienteCreate styles is added

--- src/ClienteCreate.js
+++ src/ClienteCreate.js
@@ -1,65 +1,73 @@
-import React, {Component} from 'react';
-import axios from 'axios';
+import React, { Component } from 'react'
+import axios from 'axios'
 
+import './ClienteCreate.css'
 
 class ClienteCreate extends Component {
-
   state = {
     error: null,
     errorUpdate: null,
     nombreInput: '',
     id: '',
     clientes: []
-  };
-
-  constructor(props){
-    super(props);
-
-    axios.get('https://obscure-coast-56975.herokuapp.com/cliente')
-      .then(result => {
-        const {data} = result;
-        this.setState({
-          clientes: data
-        });
-      });
   }
 
-  getNombre = (event) => {
-    const nombreInput = event.target.value;
-    this.setState({nombreInput});
-  };
+  constructor(props) {
+    super(props)
 
+    axios
+      .get('https://obscure-coast-56975.herokuapp.com/cliente')
+      .then(result => {
+        const { data } = result
+        this.setState({
+          clientes: data
+        })
+      })
+  }
 
+  getNombre = event => {
+    const nombreInput = event.target.value
+    this.setState({ nombreInput })
+  }
 
   saveClient = () => {
-    const {nombreInput} = this.state;
-    axios.post('https://obscure-coast-56975.herokuapp.com/cliente', {
-      nombre: nombreInput
-    }).then(response => {
-      this.props.history.push("/");
-    })
-    .catch(error => {
-      this.setState({error: error.message});
-    })
-  };
-
-
+    const { nombreInput } = this.state
+    axios
+      .post('https://obscure-coast-56975.herokuapp.com/cliente', {
+        nombre: nombreInput
+      })
+      .then(response => {
+        this.props.history.push('/')
+      })
+      .catch(error => {
+        this.setState({ error: error.message })
+      })
+  }
 
   render() {
-    const error = (<label>{this.state.error}</label>);
-    const errorUpdate = (<label>{this.state.errorUpdate}</label>);
+    const error = <label>{this.state.error}</label>
+    const errorUpdate = <label>{this.state.errorUpdate}</label>
     return (
-      <div className="Form">
+      <div className="Form wrapper-create-client">
         <h2>Crear Un Cliente Nuevo</h2>
-        <br/>
-        <input placeholder="Nombre " onChange={this.getNombre} value={this.state.nombreInput} />
-        <br/>
-        <button onClick={this.saveClient}>Guardar</button>
+        <br />
+        <div class="new-client-container">
+          <input
+            class="input-new-client"
+            placeholder="Nombre "
+            onChange={this.getNombre}
+            value={this.state.nombreInput}
+          />
+          <br />
+          <button class="button-save" onClick={this.saveClient}>
+            Guardar
+          </button>
+        </div>
         {this.state.error ? error : ''}
         {this.state.errorUpdate ? errorUpdate : ''}
       </div>
-    );
+    )
   }
 }
 
-export default ClienteCreate;
+export default ClienteCreate
